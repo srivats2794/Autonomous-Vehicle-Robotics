4dbb5eae63ff4e64749ddedd6ccb45f57908ec31
srivats2794 <srivats@g.clemson.edu>
2017-04-26 17:00:37 -0400

    Update rosjet_install.sh

--- master/Installation/TX1/rosjet_install.sh
+++ master/Installation/TX1/rosjet_install.sh
@@ -3,7 +3,7 @@ sudo dpkg-reconfigure tzdata
 
 #Ros Prerequisites
 sudo update-locale LANG=C LANGUAGE=C LC_ALL=C LC_MESSAGES=POSIX
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu xenial main" > /etc/apt/sources.list.d/ros-latest.list'
 wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
 sudo apt-get update
 
